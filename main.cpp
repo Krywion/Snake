c0986182194964fb47d9a5e81f20fcecb21fe86e
makskrywionek <krywionek.maks@gmail.com>
2024-05-09 09:00:22 +0200

    code refactor

--- main.cpp
+++ main.cpp
@@ -3,26 +3,19 @@
 #include "headers/Game.h"
 
 
-void exit();
 int main() {
     std::cout << "Game starting..." << std::endl;
-
-
 
     InitWindow(2 * settings::offset + settings::cellCount * settings::cellSize,
                2 * settings::offset + settings::cellCount * settings::cellSize,
                "Snake");
-    atexit(exit);
     Game game = Game();
 
     while (!WindowShouldClose()) {
         game.tick();
     }
 
-
+    CloseWindow();
     return 0;
 }
 
-void exit() {
-    CloseWindow();
-}
